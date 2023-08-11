57431835d316cc7d536c7b8a2a5d8b234320aabf
maciej <63735702+MrMaciejM@users.noreply.github.com>
2023-08-11 16:47:21 +0100

    added favicon and set title to the page

---
+++ src/Components/Header.jsx
@@ -0,0 +1,23 @@
+
+
+function Header() {
+    return (
+        <header>
+            <div>
+              <nav>
+                <a href="#">LinkedIn</a>
+                <a href="#">GitHub</a>
+                <a href="#">CV</a>
+              </nav>
+              <nav>
+                <a href="#">Projects</a>
+                <a href="#">About</a>
+                <a href="#">Contact</a>
+
+              </nav>
+            </div>
+        </header>
+    )
+}
+
+export default Header; 
