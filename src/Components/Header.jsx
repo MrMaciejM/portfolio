477f85d7bf7523865c48329a3fc312b9a9ef1602
maciej <63735702+MrMaciejM@users.noreply.github.com>
2023-08-11 18:47:37 +0100

    added icons and set template layout for the header

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,15 +1,24 @@
 import "./Header.css";
+import linkedInLogo from "../Images/linkedin-100.png"
+import githubLogo from "../Images/github-100.png"
+import cvLogo from "../Images/cv-100.png"
 
 function Header() {
     return (
         <header>
             <div>
               <nav>
-                <a href="#">LinkedIn</a>
-                <a href="#">GitHub</a>
-                <a href="#">CV</a>
+                <div>
+                <a href="#">GitHub<img className="githubLogo" src={githubLogo} alt="LinkedIn" /></a>
+                </div>
+                <div>
+                <a href="#">LinkedIn<img src={linkedInLogo} alt="LinkedIn" /></a>
+                </div>
+                <div>
+                <a href="#">CV<img className="cvLogo" src={cvLogo} alt="LinkedIn" /></a>
+                </div>                
               </nav>
-              </div>
+            </div>
             <div>
               <nav>
                 <a href="#">Projects</a>
