238e9bd20fba91982f1e608b0c6a0d2f54cf876a
maciej <63735702+MrMaciejM@users.noreply.github.com>
2023-11-29 01:44:25 +0000

    edited the title of the browser tab

--- src/Components/Projects.jsx
+++ src/Components/Projects.jsx
@@ -1,5 +1,6 @@
 import "./Projects.css"; 
 import "./Header.css"
+import cogImg from "../Assets/Images/cog.png"
 import placeholderImg from "../Assets/Images/placeholder-500x-300y.png"
 import cardChampionsImg from "../Assets/Images/cardChampions800x600.png"
 import globeTrotterImg from "../Assets/Images/globeTrotter800x600.png"
@@ -14,13 +15,19 @@ function Projects() {
           <div className="headingDecorLineR"></div>
         </div>
         <div className="projectsMainBox">
+            <div>
+                <img className="cogImg" src={cogImg} alt="cog shape" />
+                <svg className="projectsTriangle" width="100" height="100" xmlns="http://www.w3.org/2000/svg">
+                <polygon points="22.5,4.5 40.5,40.5 4.5,40.5"/>
+            </svg>
+            </div>
             <div className="cardsBox">
                 <div className="cardsImgDiv">
                     <a href="https://github.com/MrMaciejM/card-champions">
-                      <img className="projectImg" src={cardChampionsImg} alt="" />
+                      <img className="projectImg animateImg" src={cardChampionsImg} alt="" />
                     </a>
                 </div>
-                <p className="leftArrow">👈</p>
+                <p className="leftArrow">&larr;</p>
                 <div className="cardsDescDiv">
                   <h4>Card Champions &trade;</h4>
                   <p>A card game based on the ever-popular Rock, Paper, Scissors game rules. All assets and coding logic were drawn, designed and programmed by myself.</p>
